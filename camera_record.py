b6aeb452c314f816dc33089d7af6af2214f9575d
softdrink1991@gmail.com <yanetolstiy@bk.ru>
2021-11-23 02:13:53 +0300

    V1

--- camera_record.py
+++ camera_record.py
@@ -16,18 +16,20 @@ def record_video():
         ret, new_frame = capture.read()
 
         frames += 1
-        # face_location = face_recognition.face_locations(new_frame)
-        # if (len(face_location) > 0):
-        #     (top, right, bottom, left) = face_location[0]
-        #     face_image = new_frame[top:bottom, left:right]
-        #     face_image = face_pixelate(face_image, 5)
-        #     new_frame[top:bottom, left:right] = face_image
         frame = new_frame.copy()
-        # frame = cv2.cvtColor(new_frame, cv2.COLOR_BGR2GRAY)        
+        face_location = face_recognition.face_locations(frame)
+        if (len(face_location) > 0):
+            (top, right, bottom, left) = face_location[0]
+            face_image = frame[top:bottom, left:right]
+            face_image = face_pixelate(face_image, 5)
+            frame[top:bottom, left:right] = face_image
+        
+        frame = cv2.cvtColor(new_frame, cv2.COLOR_BGR2GRAY)        
         images.append(frame)
         # if frames ==0 or frames%5 == 0:
 
-        frame = cv2.resize(frame, (vid_w, vid_h))
+        # frame = cv2.resize(frame, (vid_w, vid_h))
+
             # frame = cv2.flip(frame,180)
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -38,7 +40,7 @@ def record_video():
     # print(frames)
     # print(fps)
     print(len(images)) 
-    out = cv2.VideoWriter('video.avi', fourcc, 10, (vid_w,vid_h))
+    out = cv2.VideoWriter('1.mp4', fourcc, 10, (vid_w,vid_h))
     print("creating video")
     for i in range(len(images)):
         out.write(images[i])
